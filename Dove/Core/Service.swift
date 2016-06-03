b03e084fdbfb5c3c38dbac461ccb03998c83b7e1
Contron <connorhaigh@gmail.com>
2016-06-03 23:12:56 +0100

    Added service protocol.

---
+++ Dove/Core/Service.swift
@@ -0,0 +1,13 @@
+//
+//  Service.swift
+//  Dove
+//
+//  Created by Connor Haigh on 03/06/2016.
+//  Copyright © 2016 Connor Haigh. All rights reserved.
+//
+
+import Foundation
+
+protocol Service {
+	func initialise()
+}
