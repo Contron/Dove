5f3dfdb96ad22b4a1c59979d0b1dfd2e30614178
Contron <connorhaigh@gmail.com>
2016-06-03 18:53:45 +0100

    Initial Commit

---
+++ Dove/Dove.h
@@ -0,0 +1,19 @@
+//
+//  Dove.h
+//  Dove
+//
+//  Created by Connor Haigh on 03/06/2016.
+//  Copyright © 2016 Connor Haigh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Dove.
+FOUNDATION_EXPORT double DoveVersionNumber;
+
+//! Project version string for Dove.
+FOUNDATION_EXPORT const unsigned char DoveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Dove/PublicHeader.h>
+
+
