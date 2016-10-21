6a97b08a59bdb70d620426fc5446880204cd0b4d
Contron <connorhaigh@gmail.com>
2016-10-21 10:31:23 +0100

    Added constraint methods.

---
+++ Dove/Extensions/ConstraintExtensions.swift
@@ -0,0 +1,48 @@
+//
+//  ConstraintExtensions.swift
+//  Dove
+//
+//  Created by Connor Haigh on 21/10/2016.
+//  Copyright © 2016 Connor Haigh. All rights reserved.
+//
+
+import Foundation
+
+public extension UIView {
+	public func constraintSuperview(constant: CGFloat? = nil) {
+		guard let parent = self.superview else {
+			return
+		}
+		
+		if let constant = constant {
+			self.leadingAnchor.constraint(equalTo: parent.leadingAnchor, constant: constant).isActive = true
+			self.trailingAnchor.constraint(equalTo: parent.trailingAnchor, constant: constant).isActive = true
+			self.topAnchor.constraint(equalTo: parent.topAnchor, constant: constant).isActive = true
+			self.bottomAnchor.constraint(equalTo: parent.bottomAnchor, constant: constant).isActive = true
+		} else {
+			self.leadingAnchor.constraint(equalTo: parent.leadingAnchor).isActive = true
+			self.trailingAnchor.constraint(equalTo: parent.trailingAnchor).isActive = true
+			self.topAnchor.constraint(equalTo: parent.topAnchor).isActive = true
+			self.bottomAnchor.constraint(equalTo: parent.bottomAnchor).isActive = true
+		}
+	}
+	
+	public func constraintSuperviewCenter(offset: CGSize? = nil) {
+		guard let parent = self.superview else {
+			return
+		}
+		
+		if let offset = offset {
+			self.centerXAnchor.constraint(equalTo: parent.centerXAnchor, constant: offset.width)
+			self.centerYAnchor.constraint(equalTo: parent.centerYAnchor, constant: offset.height)
+		} else {
+			self.centerXAnchor.constraint(equalTo: parent.centerXAnchor)
+			self.centerYAnchor.constraint(equalTo: parent.centerYAnchor)
+		}
+	}
+	
+	public func constraintSize(size: CGSize) {
+		self.widthAnchor.constraint(equalToConstant: size.width).isActive = true
+		self.heightAnchor.constraint(equalToConstant: size.height).isActive = true
+	}
+}
