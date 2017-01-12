b985ef78c219fcac3dbe039acb193fc1e96ce98f
Contron <connorhaigh@gmail.com>
2017-01-12 10:11:07 +0000

    Added string initialiser for dates.

--- Dove/Extensions/DateExtensions.swift
+++ Dove/Extensions/DateExtensions.swift
@@ -9,6 +9,17 @@
 import Foundation
 
 public extension Date {
+	public init?(string: String) {
+		let formatter = DateFormatter()
+		formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SZ"
+		
+		if let date = formatter.date(from: string) {
+			self.init(timeInterval: 0, since: date)
+		}
+		
+		return nil
+	}
+	
 	public func timeAgo() -> String {
 		let difference = abs(self.timeIntervalSinceNow)
 		
