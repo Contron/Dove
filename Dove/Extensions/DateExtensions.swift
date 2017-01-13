beee5042503caa27f9790ebe069e417e54c100e1
Contron <connorhaigh@gmail.com>
2017-01-13 09:32:16 +0000

    Updated date formatter.

--- Dove/Extensions/DateExtensions.swift
+++ Dove/Extensions/DateExtensions.swift
@@ -11,7 +11,9 @@ import Foundation
 public extension Date {
 	public init?(string: String) {
 		let formatter = DateFormatter()
-		formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SZ"
+		formatter.locale = Locale(identifier: "en_US_POSIX")
+		formatter.timeZone = TimeZone(abbreviation: "UTC")
+		formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
 		
 		if let date = formatter.date(from: string) {
 			self.init(timeInterval: 0, since: date)
