859e203572fae00f006d38c3f9caa0eaf573ddc3
Contron <connorhaigh@gmail.com>
2017-01-17 11:59:11 +0000

    Added an aspect fit image view.

---
+++ Dove/Views/AspectFitImageView.swift
@@ -0,0 +1,58 @@
+//
+//  AspectFitImageView.swift
+//  Dove
+//
+//  Created by Connor Haigh on 17/01/2017.
+//  Copyright © 2017 Connor Haigh. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable public class AspectFitImageView: UIImageView {
+	required public init?(coder: NSCoder) {
+		super.init(coder: coder)
+		
+		self.update()
+	}
+	
+	override public init(image: UIImage?) {
+		super.init(image: image)
+		
+		self.update()
+	}
+	
+	override public init(frame: CGRect) {
+		super.init(frame: frame)
+		
+		self.update()
+	}
+	
+	public init() {
+		super.init(frame: CGRect())
+		
+		self.update()
+	}
+	
+	private func update() {
+		guard let image = self.image else {
+			return
+		}
+		
+		if let constraint = self.constraint {
+			self.removeConstraint(constraint)
+		}
+		
+		let ratio = image.size.height / image.size.width
+		let constraint = NSLayoutConstraint(item: self, attribute: .height, relatedBy: .equal, toItem: self, attribute: .width, multiplier: ratio, constant: 0)
+		
+		self.addConstraint(constraint)
+	}
+	
+	override public var image: UIImage? {
+		didSet {
+			self.update()
+		}
+	}
+	
+	private var constraint: NSLayoutConstraint? = nil
+}
