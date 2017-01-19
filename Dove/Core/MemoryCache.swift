e3df6dda4a8795dae4eaca31b5a97461aa8504bf
Contron <connorhaigh@gmail.com>
2017-01-19 13:47:36 +0000

    Added a memory cache based on NSCache.

---
+++ Dove/Core/MemoryCache.swift
@@ -0,0 +1,30 @@
+//
+//  MemoryCache.swift
+//  Dove
+//
+//  Created by Connor Haigh on 19/01/2017.
+//  Copyright © 2017 Connor Haigh. All rights reserved.
+//
+
+import Foundation
+
+public class MemoryCache<Key, Value> where Key: Hashable {
+	public func exists(key: Key) -> Bool {
+		return self.cache.object(forKey: key as AnyObject) != nil
+	}
+	
+	public func clear() {
+		self.cache.removeAllObjects()
+	}
+	
+	public subscript(key: Key) -> Value? {
+		get {
+			return self.cache.object(forKey: key as AnyObject) as? Value
+		}
+		set {
+			self.cache.setObject(newValue as AnyObject, forKey: key as AnyObject)
+		}
+	}
+	
+	private let cache = NSCache<AnyObject, AnyObject>()
+}
