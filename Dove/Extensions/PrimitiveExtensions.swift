938debff1187ddab5397959d1c35a6f906a98bc9
Contron <connorhaigh@gmail.com>
2017-01-27 10:07:49 +0000

    Added inspect method.

--- Dove/Extensions/PrimitiveExtensions.swift
+++ Dove/Extensions/PrimitiveExtensions.swift
@@ -77,3 +77,16 @@ public extension Double {
 		return formatter.string(from: NSNumber(value: self)) ?? String(self)
 	}
 }
+
+public func inspect(_ value: Any) -> String {
+	switch value {
+	case let value as [AnyHashable: Any]:
+		return "{\(value.map({ "\($0): \(inspect($1))" }).joined(separator: ", "))}"
+	case let value as [Any]:
+		return "[\(value.map({ inspect($0) }).joined(separator: ", "))]"
+	case let value as Bool:
+		return value ? "true" : "false"
+	default:
+		return String(describing: value)
+	}
+}
