ac4c73603bba507d92f9cf29afaf5952788ab472
Contron <connorhaigh@gmail.com>
2017-10-11 13:45:59 +0100

    Updated method signature.

--- Dove/Extensions/UserDefaultsExtensions.swift
+++ Dove/Extensions/UserDefaultsExtensions.swift
@@ -9,19 +9,19 @@
 import Foundation
 
 public extension UserDefaults {
-	public func set<Type: Encodable>(_ encodable: Type, forKey key: String) {
-		guard let data = try? PropertyListEncoder().encode(encodable) else {
-			return
-		}
-		
-		self.set(data, forKey: key)
-	}
-	
-	public func decodable<Target: Decodable>(forKey key: String, _ type: Target.Type) -> Target? {
+	public func get<Target: Decodable>(decodable key: String, _ type: Target.Type) -> Target? {
 		guard let data = self.data(forKey: key), let instance = try? PropertyListDecoder().decode(type, from: data) else {
 			return nil
 		}
 		
 		return instance
+	}
+	
+	public func set<Type: Encodable>(encodable: Type, key: String) {
+		guard let data = try? PropertyListEncoder().encode(encodable) else {
+			return
+		}
+		
+		self.set(data, forKey: key)
 	}
 }
