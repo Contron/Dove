ef1c0aeaee6ea22272f35b255e11552151d71d57
Contron <connorhaigh@gmail.com>
2018-04-25 11:23:36 +0100

    Added method for rounding a date.

--- Dove/Extensions/CalendarExtensions.swift
+++ Dove/Extensions/CalendarExtensions.swift
@@ -10,15 +10,15 @@ import Foundation
 
 public extension Calendar {
 	public func startOfYear(for date: Date) -> Date {
-		return self.date(from: self.dateComponents([.era, .year], from: self.startOfDay(for: date)))!
+		return self.date(from: self.dateComponents([.year, .era], from: self.startOfDay(for: date)))!
 	}
 	
 	public func startOfMonth(for date: Date) -> Date {
-		return self.date(from: self.dateComponents([.era, .year, .month], from: self.startOfDay(for: date)))!
+		return self.date(from: self.dateComponents([.month, .year, .era], from: self.startOfDay(for: date)))!
 	}
 	
 	public func startOfWeek(for date: Date) -> Date {
-		return self.date(from: self.dateComponents([.era, .year, .month, .weekOfMonth], from: self.startOfDay(for: date)))!
+		return self.date(from: self.dateComponents([.weekOfMonth, .month, .year, .era], from: self.startOfDay(for: date)))!
 	}
 	
 	public func endOfYear(for date: Date) -> Date {
@@ -31,5 +31,9 @@ public extension Calendar {
 	
 	public func endOfWeek(for date: Date) -> Date {
 		return self.date(byAdding: DateComponents(day: -1, weekOfMonth: 1), to: self.startOfWeek(for: date), wrappingComponents: true)!
+	}
+	
+	public func date(byRounding component: Component, every value: Int, of date: Date) -> Date? {
+		return self.date(bySetting: component, value: Int(ceil(Double(self.component(component, from: date)) / Double(value))) * value, of: date)
 	}
 }
